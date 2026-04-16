0b5814b47e43d8b247c44f5d9211a0e35846be75
fayebamba12 <fayebamba252@gmail.com>
2026-04-16 15:00:04 +0000

    Fix database config

--- bibliotheque_project/settings.py
+++ bibliotheque_project/settings.py
@@ -73,10 +73,10 @@ WSGI_APPLICATION = 'bibliotheque_project.wsgi.application'
 
 # ✅ DATABASE FIX
 DATABASES = {
-    'default': dj_database_url.config(
-        conn_max_age=600,
-        ssl_require=True
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
